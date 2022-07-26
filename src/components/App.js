c9871449079532b769294d8a41df8c1029ead918
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-07-26 23:10:27 +0300

    added functions to handle the profile that when filled by a user can display a custom made greeting.

--- src/components/App.js
+++ src/components/App.js
@@ -11,21 +11,54 @@ import Profile from './Profile';
 
 function App() {
  const[isLoggedIn,setIsLoggedIn]=useState(false)
+ const[userFirstName,setUserFirstName]=useState('')
+ const[userLastName,setUserLastName]= useState('')
+ const[userBio,setUserBio]=useState('')
+ const[user,setUser]=useState({})
+
+
+ function handleFirstName(e){
+  setUserFirstName(e.target.value)
+ }
+ function handleLastName(e){
+  setUserLastName(e.target.value)
+ }
+ function handleBio(e){
+  setUserBio(e.target.value)
+ }
  
-
-
+ function handleSubmit(e){
+  e.preventDefault()
+ const userData={
+  firstName:userFirstName,
+  lastName:userLastName,
+  About:userBio
+ }
+ setUser(userData)
+ setIsLoggedIn(true)
+ 
+ }
+ 
   const[isClicked,setIsClicked]=useState(true)
   return (
     <div className='app'>
      
-      <Header isClicked={isClicked} setIsClicked={setIsClicked}/>
+      <Header isClicked={isClicked} setIsClicked={setIsClicked} user={user} isLoggedIn={isLoggedIn}/>
      <BrowserRouter>
       <NavBar />
      <Routes>
-      {/* <NavSection/> */}
+
       <Route path='/todo' element={<ToDoList/>}/>
+
       <Route path='/journal' element={<Journal/>}/>
-      <Route path='/' element={<Profile/>}/>
+
+      <Route path='/' element={<Profile
+      handleFirstName={handleFirstName}
+      handleLastName={handleLastName}
+      handleBio={handleBio}
+      handleSubmit={handleSubmit}
+      />}/>
+
       <Route path="*"  element={<h1 style={{"backgroundColor":"aliceblue"}}>Page not Found</h1>}/>
       </Routes>
     </BrowserRouter>
