90877a690c7c907019991d56ed1861c362162985
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-07-27 13:28:10 +0300

    added a todo component that will house individual todos

---
+++ src/components/ToDo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ToDo(){
+    return(
+        <div className="todo">
+            <span>&#10146; </span>
+            <h3>Go to the gym</h3>
+            <button>Remove</button>
+        </div>
+    )
+}
+
+export default ToDo
